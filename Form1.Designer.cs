d519b2be69b7839b42b7ae0230c3727e58211324
hogeinstein <hogeinstein@gmail.com>
2018-04-10 17:04:16 +0900

    It was completed tentatively.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace tsSleepBlock001
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Timer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CPU";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(54, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(41, 19);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "5";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(54, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(41, 19);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "60";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox1.Location = new System.Drawing.Point(15, 67);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(104, 24);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "START";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Min";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(129, 100);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "tsSleepBlock001";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tsSleepBlock001
+{
+    public partial class Form1 : Form
+    {
+        private PerformanceCounter _pfcounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        private int _par = -1;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Tick += CheckCpuPar;
+            float cpu = _pfcounter.NextValue();
+        }
+
+        private void CheckCpuPar(object sender, EventArgs e)
+        {
+            var cpu = (int)_pfcounter.NextValue();
+            if(cpu > _par)
+            {
+                SetThreadExecutionState(ExecutionState.SystemRequired);
+            }
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox chkBox = (CheckBox)sender;
+
+            if (chkBox.Checked)
+            {
+                var flg = true;
+                int min = -1;
+                int par = -1;
+                if (flg && !int.TryParse(textBox1.Text, out min)) flg = false;
+                if (flg && !int.TryParse(textBox2.Text, out par)) flg = false;
+                if (flg && !((min > 0) && (min < 120))) flg = false;
+                if (flg && !((par > 0) && (par < 101))) flg = false;
+                if(flg == false)
+                {
+                    MessageBox.Show("値が変です");
+                    chkBox.Checked = false;
+                    return;
+                }
+                _par = par;
+                timer1.Interval = min * 60 * 1000;
+                timer1.Start();
+                chkBox.Text = "STOP";
+            }
+            else
+            {
+                timer1.Stop();
+                chkBox.Text = "START";
+            }
+        }
+        [DllImport("kernel32.dll")]
+        extern static ExecutionState SetThreadExecutionState(ExecutionState esFlags);
+        [FlagsAttribute]
+        public enum ExecutionState : uint
+        {
+            // 関数が失敗した時の戻り値
+            Null = 0,
+            // スタンバイを抑止
+            SystemRequired = 1,
+            // 画面OFFを抑止
+            DisplayRequired = 2,
+            // 効果を永続させる。ほかオプションと併用する。
+            Continuous = 0x80000000,
+        }
+    }
+}
